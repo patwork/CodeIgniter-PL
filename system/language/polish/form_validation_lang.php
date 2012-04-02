38311a4118c1172642c333880019708960a53def
patwork <patwork@gmail.com>
2012-04-02 07:39:39 +0200

    updated to 3.0-dev (migration & form_validation: is_unique, greater_than_equal_to, less_than_equal_to)

--- system/language/polish/form_validation_lang.php
+++ system/language/polish/form_validation_lang.php
@@ -9,28 +9,31 @@
  * @since		Version 1.0
  */
 
-$lang['alpha']              = "Pole %s może zawierać tylko litery.";
-$lang['alpha_dash']         = "Pole %s może zawierać tylko litery, cyfry, podkreślenia i myślniki.";
-$lang['alpha_numeric']      = "Pole %s może zawierać tylko litery i cyfry.";
-$lang['decimal']            = "Pole %s musi zawierać liczbę dziesiętną.";
-$lang['exact_length']       = "Pole %s musi zawierać dokładnie %s znaków.";
-$lang['greater_than']       = "Pole %s musi zawierać liczbę większą niż %s.";
-$lang['integer']            = "Pole %s musi zawierać liczbę całkowitą.";
-$lang['is_natural']         = "Pole %s musi zawierać liczbę naturalną.";
-$lang['is_natural_no_zero'] = "Pole %s musi zawierać liczbę większą od zera.";
-$lang['is_numeric']         = "Pole %s musi zawierać liczbę.";
-$lang['isset']              = "Pole %s musi mieć wartość.";
-$lang['less_than']          = "Pole %s musi zawierać liczbę mniejszą niż %s.";
-$lang['matches']            = "Pole %s ma inną wartość niż pole %s.";
-$lang['max_length']         = "Pole %s nie może zawierać więcej niż %s znaków.";
-$lang['min_length']         = "Pole %s musi zawierać co najmniej %s znaków.";
-$lang['numeric']            = "Pole %s musi zawierać liczbę.";
-$lang['regex_match']        = "Pole %s ma nieprawidłowy format.";
-$lang['required']           = "Pole %s jest wymagane.";
-$lang['valid_email']        = "Pole %s musi zawierać poprawny adres email.";
-$lang['valid_emails']       = "Pole %s musi zawierać poprawne adresy email.";
-$lang['valid_ip']           = "Pole %s musi zawierać poprawny IP.";
-$lang['valid_url']          = "Pole %s musi zawierać poprawny URL.";
+$lang['alpha']                 = "Pole %s może zawierać tylko litery.";
+$lang['alpha_dash']            = "Pole %s może zawierać tylko litery, cyfry, podkreślenia i myślniki.";
+$lang['alpha_numeric']         = "Pole %s może zawierać tylko litery i cyfry.";
+$lang['decimal']               = "Pole %s musi zawierać liczbę dziesiętną.";
+$lang['exact_length']          = "Pole %s musi zawierać dokładnie %s znaków.";
+$lang['greater_than']          = "Pole %s musi zawierać liczbę większą niż %s.";
+$lang['greater_than_equal_to'] = "Pole %s musi zawierać liczbę większą lub równą %s.";
+$lang['integer']               = "Pole %s musi zawierać liczbę całkowitą.";
+$lang['is_natural']            = "Pole %s musi zawierać liczbę naturalną.";
+$lang['is_natural_no_zero']    = "Pole %s musi zawierać liczbę większą od zera.";
+$lang['is_numeric']            = "Pole %s musi zawierać liczbę.";
+$lang['is_unique']             = "Pole %s musi zawierać wartość unikatową.";
+$lang['isset']                 = "Pole %s musi mieć wartość.";
+$lang['less_than']             = "Pole %s musi zawierać liczbę mniejszą niż %s.";
+$lang['less_than_equal_to']    = "Pole %s musi zawierać liczbę mniejszą lub równą %s.";
+$lang['matches']               = "Pole %s ma inną wartość niż pole %s.";
+$lang['max_length']            = "Pole %s nie może zawierać więcej niż %s znaków.";
+$lang['min_length']            = "Pole %s musi zawierać co najmniej %s znaków.";
+$lang['numeric']               = "Pole %s musi zawierać liczbę.";
+$lang['regex_match']           = "Pole %s ma nieprawidłowy format.";
+$lang['required']              = "Pole %s jest wymagane.";
+$lang['valid_email']           = "Pole %s musi zawierać poprawny adres email.";
+$lang['valid_emails']          = "Pole %s musi zawierać poprawne adresy email.";
+$lang['valid_ip']              = "Pole %s musi zawierać poprawny IP.";
+$lang['valid_url']             = "Pole %s musi zawierać poprawny URL.";
 
 /* End of file form_validation_lang.php */
 /* Location: ./system/language/polish/form_validation_lang.php */
---
+++ system/language/polish/migration_lang.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CodeIgniter Polish language pack
+ *
+ * @package		CodeIgniter
+ * @author		patwork@gmail.com (originally sin@sinsoft.pl)
+ * @license		http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link		https://github.com/patwork/CodeIgniter-PL
+ * @since		Version 1.0
+ */
+
+$lang['migration_class_doesnt_exist']  = "Klasa migracji \"%s\" nie może być znaleziona.";
+$lang['migration_invalid_filename']    = "Migracja \"%s\" ma błędną nazwę pliku.";
+$lang['migration_missing_down_method'] = "Klasa migracji \"%s\" nie posiada metody \"down\".";
+$lang['migration_missing_up_method']   = "Klasa migracji \"%s\" nie posiada metody \"up\".";
+$lang['migration_multiple_version']    = "Znaleziono kilka migracji o tym samym numerze: %d.";
+$lang['migration_none_found']          = "Nie znaleziono żadnej migracji.";
+$lang['migration_not_found']           = "Rządana migracja nie może być znaleziona.";
+
+/* End of file migration_lang.php */
+/* Location: ./system/language/polish/migration_lang.php */
